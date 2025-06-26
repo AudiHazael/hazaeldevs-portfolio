50667cc05095a0293fc5f84fca16380534b4d0b4
Hazael Audi <hazaelaudi@hmail.com>
2025-06-26 22:24:10 +0100

    Temp Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Thanks from "./pages/thank-you";
 import Error from "./pages/Error";
-// import Temporary from "./pages/Temporary";
+import Temporary from "./pages/TempPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Temporary />} /> */}
-        <Route path="/Temporary" element={<Temporary />} />
+        <Route path="/" element={<Temporary />} />
+        <Route path="/TempPage" element={<Temporary />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/thank-you" element={<Thanks />} />
         <Route path="*" element={<Error />} />

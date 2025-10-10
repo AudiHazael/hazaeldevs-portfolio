ba9fcc6ae626a9741d5d09820bb974f4eb6d4d9f
Hazael Audi <hazaelaudi@hmail.com>
2025-10-10 16:10:21 +0100

    More UI Fixes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -108,7 +108,7 @@ function Services() {
         </motion.div>
 
         {/* --- Feature Grid --- */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {[
             {
               icon: Icons.CodeIcon,
